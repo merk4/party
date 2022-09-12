aa9a2b1c7c04fa27bd1621074632a235d2646ed8
Nikos Merk <nikos97merk@gmail.com>
2022-09-12 15:50:30 +0300

    changed button

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+
+// ✅ Change button text on click
+btn.addEventListener('click', function handleClick() {
+  btn.textContent = 'Saturday 17.09.22 23:00';
+});
