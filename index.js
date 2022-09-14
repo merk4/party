8b0d527c3a626ae335fa77248f28d7c4e2d35699
Nikolaos Merkouris <34164921+merk4@users.noreply.github.com>
2022-09-14 18:18:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const btn = document.getElementById('btn');
 
 // ✅ Change button text on click
 btn.addEventListener('click', function handleClick() {
-  btn.textContent = 'Saturday 17.09.22 23:23';
+  btn.textContent = 'Saturday 17.09.22 22:22';
 });
