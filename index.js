32976a96997866134e713aaecf467d2b1c4259f5
Nikolaos Merkouris <34164921+merk4@users.noreply.github.com>
2023-09-22 18:10:52 +0300

    Revert "Added Invitations"
    This reverts commit be454f98e93c2871e064b98c7e9b6a2266b4a15d.

--- index.js
+++ index.js
@@ -79,9 +79,6 @@ function updateTextBasedOnId(id) {
     '12332' : 'Alexandra', // https://merk4.github.io/party/?id=12332
     '878341': 'Vivi', // https://merk4.github.io/party/?id=878341
     '878342': 'Eleftheria', // https://merk4.github.io/party/?id=878342
-    '180902': 'Theodore', // https://merk4.github.io/party/?id=180902
-    '180903': 'Zgou', // https://merk4.github.io/party/?id=180903
-    '180904': 'Christo', // https://merk4.github.io/party/?id=180904
     '432144': 'Evelina' // https://merk4.github.io/party/?id=432144
 
   };
